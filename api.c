d43ac9c5b2f6ae0a94376ef0907374ce106f1223
cptclvtch <81629275+cptclvtch@users.noreply.github.com>
2023-10-21 12:24:03 -0600

    Decoupled event handling

--- api.c
+++ api.c
@@ -91,29 +91,20 @@ uint8_t poll_event(EVENT* event)
   return SDL_PollEvent(event);
 }
 
-void standard_loop()
+void standard_loop(void (*event_handling)(EVENT* event))
 {
   bool running = true;
-  EVENT event;
   while(running)
   {
-    // Process events
+    //Its best to handle the input first
+    EVENT event;
+    nk_input_begin(ctx);
     while(poll_event(&event))
     {
-      if(event.type == SDL_QUIT)
-      {
-        running = false;
-      }
-      else
-      if(event.type == SDL_KEYDOWN)
-      {
-        const char *key = SDL_GetKeyName(event.key.keysym.sym);
-        if(strcmp(key, "Q") == 0)
-        {
-          running = false;
-        }
-      }
+      event_handling(&event);
+      nk_sdl_handle_event(&event);
     }
+    nk_input_end(ctx);
 
     // Clear screen
 
--- setup/main.c
+++ setup/main.c
@@ -1,10 +1,27 @@
 #include "crossplatform_app/api.c"
 
+void (*event_handling)(EVENT* event)
+{
+      if(event.type == SDL_QUIT)
+      {
+        running = false;
+      }
+      else
+      if(event.type == SDL_KEYDOWN)
+      {
+        const char *key = SDL_GetKeyName(event.key.keysym.sym);
+        if(strcmp(key, "Q") == 0)
+        {
+          running = false;
+        }
+      }
+}
+
 int main()
 {
     setup();
 
-    standard_loop();
+    standard_loop(event_handling);
 
     close();
 }
