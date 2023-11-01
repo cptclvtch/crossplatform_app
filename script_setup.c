86581dbb413bc6327c300b0bbbb02ec73630e382
cptclvtch <81629275+cptclvtch@users.noreply.github.com>
2023-11-01 07:32:37 -0600

    Gutted out all the git stuff

--- script_setup.c
+++ script_setup.c
@@ -11,15 +11,4 @@ int main()
     #if defined __APPLE__ || __linux__
     system("cp -R ./setup/* ../");
     #endif
-
-    //git setup
-    char cwd[512];
-    char formatted_command[512];
-    get_cwd(cwd, sizeof(cwd));    
-    sprintf(formatted_command, "git config --global --add safe.directory %s", cwd);
-    printf("Making the current directory safe for git.\n %s\n\n",formatted_command);
-    system(formatted_command);
-    
-    system("git submodule init");
-    system("tcc ../script_update_api.c -run");
 }
